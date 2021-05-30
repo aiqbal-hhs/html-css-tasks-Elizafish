64c8f14c9b52fef75d42a87bea3ede920638d49a
Elizafish <80366280+Elizafish@users.noreply.github.com>
2021-05-31 10:39:43 +1200

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,6 @@
+#To write a variable youmust have an appropriate name for it not just the first letter.
+name = input("Hi, what is your name?")
+favourite_food = input("Whats your favourite food?")
+favourite_band = input("What is your favourite band?")
+
+print("Hi {}, I bet you'd like to listen to {} while eating {}" .format(name, favourite_band, favourite_food))
