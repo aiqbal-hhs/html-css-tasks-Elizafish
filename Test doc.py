e78e7d4d10f1722f5da3a4e629bd47eb01dabc0d
Elizafish <80366280+Elizafish@users.noreply.github.com>
2021-06-03 14:45:12 +1200

    Add files via upload

--- Test doc.py
+++ Test doc.py
@@ -5,3 +5,14 @@ print("0110100001100101011011000110110001101111")
 name = input("What is your full name?")
 age = input("Where do you live?")
 print("So your name is {} and you live at {}." .format (name, age))
+
+a = 33
+b = 32
+if b > a:
+    print("'b' is bigger than 'a'.")
+elif b < a:
+    print("'b' is smaller than 'a'.")
+else:
+    print("'b' is equal to 'a'.")
+
+print("{}".format (name))
